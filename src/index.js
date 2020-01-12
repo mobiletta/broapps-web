075ec20a7986b9f7778e845eb50ef7632c20424f
Brian Weber <brian@mobiletta.com>
2020-01-12 07:44:35 -0600

    mount point adjustment at root

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
+import 'core-js';
+import 'core-js/modules/es.promise';
+import 'core-js/modules/es.array.iterator';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
-import { store, firestoreProps } from './store/createReduxStore';
-import Root from './Root'
+import { store, firestoreProps } from '@/store/createReduxStore';
+import Root from './Root';
 
 ReactDOM.render(
   <Root>
-  <Provider store={store}>
-    <ReactReduxFirebaseProvider {...firestoreProps}>
-      <App />
-    </ReactReduxFirebaseProvider>
-  </Provider></Root> , document.getElementById('root'));
+    <Provider store={store}>
+      <ReactReduxFirebaseProvider {...firestoreProps}>
+        <App />
+      </ReactReduxFirebaseProvider>
+    </Provider>
+  </Root>,
+  document.getElementById('root')
+);
 registerServiceWorker();
-
-
