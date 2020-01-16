3d40475759352e74d89a7910fdbb10971c63006b
Brian Weber <brian@mobiletta.com>
2020-01-16 10:35:52 -0600

    added new video to landing page in production

--- src/components/auth/Landing.js
+++ src/components/auth/Landing.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { FullsizePicture } from 'react-responsive-picture';
 import VideoBg from 'reactjs-videobg';
 import splash from '../../assets/images/allin-premier-splash-1920w.png'; // with import
-import bgvideo from '../../assets/video/4k-aerial-birds-eye-view-of-las-vegas-strip-at-night_hwrmqckh__D4k.mp4';
+import bgvideo from '../../assets/video/broleans-app-intro.mp4';
 import poster from '../../assets/images/aeriallv-poster.png';
 
 export default class Landing extends Component {
