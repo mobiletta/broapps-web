5568635c7be351c061c15d395b4c54957a1639e9
Brian Weber <brian@mobiletta.com>
2020-01-30 17:42:54 -0600

    used a different responsive video wrapper, other component didn't work

--- src/components/auth/ResponsiveVideo.jsx
+++ src/components/auth/ResponsiveVideo.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import ReactPlayer from 'react-player';
+import poster from '../../assets/images/intro-assets-adjust-1920-poster.png'
 import styles from './ResponsiveVideo.module.scss';
-import url from '../../assets/video/intro-assets-adjust-1920-c.mp4';
+
+import { DefaultPlayer as Video } from 'react-html5video';
+import 'react-html5video/dist/styles.css';
 
 export default class ResponsiveVideo extends Component {
   render() {
     return (
-      <div className={styles.responsiveVideoWrapper}>
-        <ReactPlayer url={url} className={styles.responsiveVideo} playing loop width="100%" height="100%" />
+      <div className={styles.pushToBottom}>
+        <Video autoPlay loop
+            controls={['Volume','Fullscreen']}
+            poster={poster}>
+            <source src='https://bro-media.s3.amazonaws.com/static-video/intro-assets-adjust-1920-c.mp4' type='video/mp4' />
+        </Video>
       </div>
     );
   }
