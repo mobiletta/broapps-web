fd9e2c2ea79451cd0d8fd3d0bbf41c265ac8e9d7
Brian Weber <brian@mobiletta.com>
2020-01-30 04:31:14 -0600

    assets updated and landing page now fully responsive

--- src/components/auth/Landing.js
+++ src/components/auth/Landing.js
@@ -1,19 +1,49 @@
 import React, { Component } from 'react';
-import { FullsizePicture } from 'react-responsive-picture';
-import VideoBg from 'reactjs-videobg';
-import splash from '../../assets/images/allin-premier-splash-1920w.png'; // with import
-import bgvideo from '../../assets/video/broleans-app-intro.mp4';
-import poster from '../../assets/images/aeriallv-poster.png';
+import ReactPlayer from 'react-player';
+//import mp4url from '../../assets/video/intro-assets-adjust-1920-c.mp4';
+//import webmurl from '../../assets/video/intro-assets-adjust-1920.webm';
+//import posterurl from '../../assets/images/aeriallv-poster.png';
+import '../../index.css';
+
+class ResponsivePlayer extends React.Component {
+  render() {
+    return (
+      <div className="player-wrapper">
+        <ReactPlayer
+          url="https://bro-media.s3.amazonaws.com/static-video/intro-assets-adjust-1920-c.mp4"
+          className="react-player"
+          playing
+          loop
+          width="100%"
+          height="100%"
+        />
+      </div>
+    );
+  }
+}
+
+/* function ResponsiveImage( { src, width, height } ) {
+  return (
+    <div
+      style={ { 
+        width,
+      } }
+      className="responsive-image">
+      <div style={ {
+          paddingBottom: ( height / width * 100 ) + '%'
+        } } />
+        <VideoBg poster={poster}>
+            <VideoBg.Source src={bgvideo} type="video/mp4" />
+        </VideoBg>
+      <img
+        src={ src }
+        className="responsive-image__image" />
+    </div>
+  );
+} */
 
 export default class Landing extends Component {
   render() {
-    return (
-      <div style={{ height: '105vh' }}>
-        <VideoBg poster={poster}>
-          <VideoBg.Source src={bgvideo} type="video/mp4" />
-        </VideoBg>
-        <FullsizePicture src={splash} />
-      </div>
-    );
+    return <ResponsivePlayer />;
   }
 }
