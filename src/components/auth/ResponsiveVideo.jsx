83e321549d8549cec7180614b85e94cb03d9074b
Brian Weber <brian@mobiletta.com>
2020-01-30 10:27:18 -0600

    switched back to local media stream

--- src/components/auth/ResponsiveVideo.jsx
+++ src/components/auth/ResponsiveVideo.jsx
@@ -1,20 +1,13 @@
 import React, { Component } from 'react';
 import ReactPlayer from 'react-player';
 import styles from './ResponsiveVideo.module.scss';
-//import url from '../../assets/video/intro-assets-adjust-1920.webm'
+import url from '../../assets/video/intro-assets-adjust-1920-c.mp4';
 
 export default class ResponsiveVideo extends Component {
   render() {
     return (
       <div className={styles.responsiveVideoWrapper}>
-        <ReactPlayer
-          url="https://bro-media.s3.amazonaws.com/static-video/intro-assets-adjust-1920.webm"
-          className={styles.responsiveVideo}
-          playing
-          loop
-          width="100%"
-          height="100%"
-        />
+        <ReactPlayer url={url} className={styles.responsiveVideo} playing loop width="100%" height="100%" />
       </div>
     );
   }
